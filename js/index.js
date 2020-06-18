eb369b9e4284936dd0b0f9f7cd531a5cd865a822
Tzikas <niko.tzikas@gmail.com>
2020-06-18 12:38:50 -0400

    first steps

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
 
-  function startGame() {}
+document.getElementById('start-button').onclick = () => {
+  startGame();
 };
+
+
+
+function startGame() {}
+
+
+
+let canvas = document.querySelector('#canvas')
+let ctx = canvas.getContext('2d')
+
+
+ctx.fillStyle = 'green'
+ctx.fillRect(10,10,1000,1000)
+
+
+let carImage = new Image() 
+carImage.src = '../images/car.png'
+carImage.onload = function(e){
+  ctx.drawImage(carImage,100,100,40,100)
+}
+
+let carObj = {
+
+}
+
+
